44eb7980b38089d8048216478f9295a887298e2a
Lussati Santos <lussatisantos10@gmail.com>
2024-05-29 21:50:28 +0100

    .

--- healing/settings.py
+++ healing/settings.py
@@ -106,6 +106,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'templates/static'),)
+#STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
